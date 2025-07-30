f7528429bc156fd2cdb870d437dc078d4baf0a27
Ahmed Hamed <ahamed@li-4c4c4544-0033-4210-8031-b5c04f525733.ibm.com>
2025-07-30 14:36:06 -0400

    updated db connection string

--- app/database.py
+++ app/database.py
@@ -1,5 +1,5 @@
 from databases import Database
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:secret@localhost/tricky")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://tricky_user_dev:MGTGZWyHmlCcFDd5U6RXuQugXCBlOWTY@dpg-d256537gi27c73bntfk0-a.oregon-postgres.render.com/tricky_turns_dev_db")
 database = Database(DATABASE_URL)
